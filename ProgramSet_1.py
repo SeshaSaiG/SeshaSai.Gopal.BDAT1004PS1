be9c99fc22a3d872cbaf261e5a3db5163076c270
SeshaSaiG <106537663+SeshaSaiG@users.noreply.github.com>
2022-05-30 18:55:09 +0530

    Add files via upload
    Problem Set 1-Inprogress

---
+++ ProgramSet_1.py
@@ -0,0 +1,28 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(type(5)
+      a=5
+print(type(a)
+      
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+a=5
+print(type(a))
+      
+SyntaxError: multiple statements found while compiling a single statement
+a=8
+      
+ptint(type(a))
+      
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    ptint(type(a))
+NameError: name 'ptint' is not defined. Did you mean: 'print'?
+a=8
+      
+print(type(a))
+      
+<class 'int'>
+a=5
+print(type(a))
+      
+SyntaxError: multiple statements found while compiling a single statement
